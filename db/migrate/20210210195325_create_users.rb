3906d78fa82df6dc8ba24b26585b67ec4761793f
Miguel Valdez <mike.281299@gmail.com>
2021-02-10 14:07:33 -0700

    Modified password name

--- db/migrate/20210210195325_create_users.rb
+++ db/migrate/20210210195325_create_users.rb
@@ -2,9 +2,9 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :email
-      t.string :password
-      t.text :name
-      t.text :lastname
+      t.string :password_digest
+      t.string :name
+      t.string :lastname
       t.integer :num_posts
       t.integer :resolved_posts
 
