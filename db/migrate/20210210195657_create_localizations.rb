7f120e3e60af2d9266c39f321a9bf92998b57387
Miguel Valdez <mike.281299@gmail.com>
2021-02-10 16:25:28 -0700

    Enabled UUID

--- db/migrate/20210210195657_create_localizations.rb
+++ db/migrate/20210210195657_create_localizations.rb
@@ -1,6 +1,6 @@
 class CreateLocalizations < ActiveRecord::Migration[6.0]
   def change
-    create_table :localizations do |t|
+    create_table :localizations, id: :uuid do |t|
       t.string :X
       t.string :Y
 
--- db/migrate/20210210201454_create_comments.rb
+++ db/migrate/20210210201454_create_comments.rb
@@ -1,6 +1,6 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
-    create_table :comments do |t|
+    create_table :comments, id: :uuid do |t|
       t.text :body
       t.timestamps
     end
