7f65f008d0d81a944f39bb69dc65e1755d7cfd7a
Miguel Valdez <mike.281299@gmail.com>
2021-02-20 13:57:17 -0700

    Minor changes to application_controler authorization method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,12 @@ class ApplicationController < ActionController::API
             render json: {message: "Unauthorized"}, status: :unauthorized
         else
         token = authorization_header.split(" ")[1]
-        secret_key = Rails.application.secrets.secret_key_base[0]
-        decoded_token = JWT.decode(token, secret_key)[0]
+        payload = JsonWebToken.decode(token)
         
         @user = User.find(decoded_token["user_email"])
         end
-    end
-
-    def authenticate!
-        token = request.headers['Authorization']&.split(' ').last
-        payload = JsonWebToken.decode(token)
-        @current_user = User.find(payload[:user_id])
-
-    rescue JWT::DecodeError => e 
+        
+    rescue JWT::DecodeError => e
         render json: {errors: e.message}, status: :forbidden  
     end
 end
