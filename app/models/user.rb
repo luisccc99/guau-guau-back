328cf5a426dccd61fdd47bb2992ce3cd7fc9a194
Miguel Valdez <mike.281299@gmail.com>
2021-03-06 14:52:15 -0700

    Added aboutme in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
     validates :email, uniqueness: true
     validates :email, :password_digest, :name, :lastname, presence: true
     validates :name, :lastname, length:   {in: 2..13}
-    
+    validates :aboutme, length: {maximum: 150}
     has_many :posts
     has_many :comments, through: :posts
 end
