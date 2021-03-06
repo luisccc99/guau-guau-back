36eb6ce9862e7f270aa7b447139c6a0403fee75d
Miguel Valdez <mike.281299@gmail.com>
2021-03-06 15:06:46 -0700

    Returning user.id on auth controller

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -6,7 +6,12 @@ class Api::V1::AuthController < ApplicationController
             render json: {error: "Invalid user."}, status: :conflict
             elsif user.authenticate(params[:password])
                 @token, exp = JsonWebToken.encode({user_email: user.email})
-                render json: {token: @token, message: "Correct credentials.", exp: exp.strftime('%d-%m-%Y %H:%M:%S')}, status: :ok
+                render json: {
+                    user_id: user.id, 
+                    token: @token, message: "Correct credentials.",
+                    exp: exp.strftime('%d-%m-%Y %H:%M:%S')
+                }, 
+                    status: :ok
             else
                 render json: {message: "Wrong credentials."}, status: :unauthorized
         end
