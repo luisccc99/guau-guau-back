ee9be710e913b4dd05744058fa76421dd34ef14a
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2021-05-20 00:21:09 -0600

    Changed token time to 10 min (TEST ONLY)

--- app/concerns/JsonWebToken.rb
+++ app/concerns/JsonWebToken.rb
@@ -1,6 +1,6 @@
 class JsonWebToken
     @secret_key = ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base[0]
-    def self.encode(payload, exp = 720.minutes.from_now)
+    def self.encode(payload, exp = 10.minutes.from_now)
         payload[:exp] = exp.to_i
         @token = JWT.encode(payload, @secret_key)
         [@token, exp]
